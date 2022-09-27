6ae506621212520fc6f0af1de5401d6f125a41a5
ahmetozkanio <bledwarrior@gmail.com>
2022-09-27 19:22:49 +0300

    TabBar icon added

--- TravelGuideApp/Screens/HomePages/Home/View/HomeViewController.swift
+++ TravelGuideApp/Screens/HomePages/Home/View/HomeViewController.swift
@@ -11,6 +11,8 @@ class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+     
+       
     }
 
     
--- TravelGuideApp/Screens/HomePages/TabBar/TabBarViewController.swift
+++ TravelGuideApp/Screens/HomePages/TabBar/TabBarViewController.swift
@@ -11,12 +11,18 @@ final class TabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+
         loadTabs()
     }
     private func loadTabs(){
+        self.tabBar.tintColor = .placeholderText
+        
+        
            let homeViewStoryboard = UIStoryboard(name: "HomeView", bundle: nil)
            let homeViewController = homeViewStoryboard.instantiateInitialViewController()!
-           
+        
+      
            let searchViewStoryboard = UIStoryboard(name: "SearchView", bundle: nil)
            let searchViewController = searchViewStoryboard.instantiateInitialViewController()!
            
